c98f199005068f5b80410a9e9db81b39ec5d3ac6
Fabian <fabikostrzewski@gmai.com>
2024-10-13 18:01:13 +0200

    Kod z przed makemigrations 13.10 z domu

--- strona/notatki/models.py
+++ strona/notatki/models.py
@@ -1,16 +1,27 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Note(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'roboczy', 'Roboczy'
+        PUBLISHED = 'opublikowany', 'Opublikowany'
+
     title = models.CharField(max_length=255)
-    body = models.TextField()
     slug = models.SlugField(max_length=255, unique=True)
-    created_at = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='notatki_notes',)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
     class Meta:
-        ordering = ['-created_at']
+        ordering = ['-publish']
         indexes = [
-            models.Index(fields=['-created_at'])
+            models.Index(fields=['-publish'])
                    ]
     def __str__(self):
         return self.title
