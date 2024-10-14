1cf6aa90639d67b3561df33f4b04aa9c2c4763b1
Fabian <fabikostrzewski@gmai.com>
2024-10-14 19:47:46 +0200

    Dopracowanie urlow, poprawne połączenie widokow, uporzadkowanie "szablonu" na poszczegolne linki

--- strona/notatki/urls.py
+++ strona/notatki/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 app_name = 'notatki'
 urlpatterns = [
- path('', views.notes_list, name='notes_list'),
- path('<int:id>/', views.notes_detail, name='notes_detail'),
+ path('', views.note_list, name='note_list'),
+ path('<slug:slug>/', views.note_detail, name='note_detail'),
 ]#
--- strona/notatki/views.py
+++ strona/notatki/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from .models import Note
 from django.http import Http404
 # Create your views here.
-def notes_list(request):
+def note_list(request):
  notes = Note.published.all()
  return render(request,
  'notatki/note/list.html',
  {'notes': notes})
-def notes_detail(request, id):
+def note_detail(request, slug):
     note = get_object_or_404(Note,
-                             id=id,
+                             slug=slug,
                              status=Note.Status.PUBLISHED)
     return render(request,
  'notatki/note/list.html',
